93030abcd7bce00f8b49af0d39dd690561a01816
Bassem Kamal <bassem_kamal@epam.com>
2025-01-04 00:12:34 +0100

    Add Summary to API

--- HackerNews.WebApi/Controllers/StoriesController.cs
+++ HackerNews.WebApi/Controllers/StoriesController.cs
@@ -1,5 +1,4 @@
 ﻿using HackerNews.Services.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
@@ -16,6 +15,12 @@ namespace HackerNews.WebApi.Controllers
             _storyService = storyService;
         }
 
+        /// <summary>
+        /// Retrieves a specified number of top stories.
+        /// </summary>
+        /// <param name="count">The number of stories to retrieve. Must be greater than 0. Default value is 10.</param>
+        /// <response code="200">Returns the requested number of top stories</response>
+        /// <response code="400">If count is less than or equal to 0</response>
         [HttpGet]
         public async Task<IActionResult> GetTopStories([FromQuery][Range(1, int.MaxValue, ErrorMessage = "Count must be a positive number.")] int count = 10)
         {
