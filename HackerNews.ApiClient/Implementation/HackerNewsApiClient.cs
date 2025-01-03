a5c1174b7c5434b314487796246a97d61c833b47
Bassem_Kamal <Bassem_Kamal@EPPLKRAW04EF>
2025-01-03 22:25:47 +0100

    Add project files.

---
+++ HackerNews.ApiClient/Implementation/HackerNewsApiClient.cs
@@ -0,0 +1,24 @@
+﻿using HackerNews.ApiClient.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerNews.ApiClient.Implementation
+{
+    public class HackerNewsApiClient : IHackerNewsApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HackerNewsApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<int>> GetBestStoryIdsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<int>>("https://hacker-news.firebaseio.com/v0/beststories.json");
+        }
+    }
+}
---
+++ HackerNews.ApiClient/Interfaces/IHackerNewsApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerNews.ApiClient.Interfaces
+{
+    public interface IHackerNewsApiClient
+    {
+        Task<IEnumerable<int>> GetBestStoryIdsAsync();
+        //Task<Story> GetStoryByIdAsync(int id);
+    }
+}
